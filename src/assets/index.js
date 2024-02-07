f931d353b9fa95cd45fb5aa8be8e61e520c3d137
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-07 12:57:47 +0530

    Setup files

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import IconList from "./images/icon-list.svg";
+import IconSucess from "./images/icon-success.svg";
+import ImageDesktopSignUp from "./images/illustration-sign-up-desktop.svg";
+import ImageMobileSignUp from "./images/illustration-sign-up-mobile.svg";
+
+export { IconList, IconSucess, ImageDesktopSignUp, ImageMobileSignUp };
